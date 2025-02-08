69f9842306b74b4fc664b1f4795eb33a1dfdda3d
Kirk Lin <linkirk@163.com>
2025-02-08 17:46:58 +0800

    style: update style

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,4 +1,5 @@
-import { cfuse, type ClassValue } from "cfuse";
+import type { ClassValue } from "cfuse";
+import { cfuse } from "cfuse";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
