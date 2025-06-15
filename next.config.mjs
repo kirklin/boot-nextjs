fd945e1e9bd7ca0e2c905aac59f1a105248567cb
Kirk Lin <linkirk@163.com>
2025-06-15 20:22:37 +0800

    refactor: routing for legal pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ import withNextIntl from "next-intl/plugin";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your existing Next.js configuration options go here
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 // Wrap the config with the next-intl plugin
--- src/app/[locale]/(legal)/components/container.tsx
+++ src/app/[locale]/(legal)/components/container.tsx
@@ -5,7 +5,10 @@ interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
 export function Container({ className, ...props }: ContainerProps) {
   return (
     <div
-      className={cn("container mx-auto px-4 sm:px-6 lg:px-8", className)}
+      className={cn(
+        "prose dark:prose-invert container mx-auto px-4 sm:px-6 lg:px-8",
+        className,
+      )}
       {...props}
     />
   );
