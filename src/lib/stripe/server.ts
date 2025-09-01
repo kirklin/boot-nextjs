e89cefbaabde821e71819d6358b87bf2800d5ae0
Kirk Lin <linkirk@163.com>
2025-09-01 17:03:15 +0800

    fix: stripe api version

--- src/lib/stripe/server.ts
+++ src/lib/stripe/server.ts
@@ -3,7 +3,7 @@ import { env } from "~/config/server";
 
 export const stripe = env.STRIPE_SECRET_KEY
   ? new Stripe(env.STRIPE_SECRET_KEY, {
-    apiVersion: "2025-05-28.basil",
+    apiVersion: "2025-08-27.basil",
     typescript: true,
   })
   : null;
